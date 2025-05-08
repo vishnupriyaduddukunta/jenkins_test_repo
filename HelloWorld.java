f6f50c33a738695b9571dcb90982ce6e8553bed0
vishnupriyaduddukunta <137520122+vishnupriyaduddukunta@users.noreply.github.com>
2025-05-08 18:31:21 -0400

    Updated the message in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from Java and Jenkins!");
+        System.out.println("Hello from Jenkins!");
     }
 }
